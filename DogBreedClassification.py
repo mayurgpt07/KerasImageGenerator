7d16e9b755b8d51259dfa164167d2dc92cb619bc
mayurgpt07 <mayurgpt07@gmail.com>
2020-09-14 23:17:09 -0700

    Pooling require different structre

--- DogBreedClassification.py
+++ DogBreedClassification.py
@@ -38,8 +38,9 @@ test_images = datagen_test.flow_from_directory(
         classes=['test'])
         
 input_tensor = Input(shape=(224,224,3))        
-model_resnet = ResNet50(include_top=False, input_tensor=input_tensor,input_shape=(224,224,3) ,weights = None, classes=120)
+model_resnet = ResNet50(include_top=False, input_tensor=input_tensor,input_shape=(224,224,3),weights = None, classes=120)
 
+print(model_resnet.summary())
 conv1 = Conv2D(256, (3,3), activation = 'relu')(model_resnet.output)
 pool1 = MaxPooling2D(2,2)(conv1)
 drop1 = Dropout(0.2)(pool1)
@@ -50,8 +51,8 @@ fc2 = Dense(120, activation='softmax')(flatten1)
 model = Model(inputs=model_resnet.input, outputs=fc2)
 
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['acc'])
-model.fit(train_images, epochs = 20, batch_size = 64)
-# print(model.summary())
+# model.fit(train_images, epochs = 20, batch_size = 64)
+print(model.summary())
 # model.fit_generator(
 #     train_images,
 #     steps_per_epoch = 700,
