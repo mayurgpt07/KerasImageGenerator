6f8e4faed2e1118a924b84693f926b39283d7724
mayurgpt07 <mayurgpt07@gmail.com>
2020-09-19 22:34:29 -0700

    Working Example of Keras Image Generator

--- DogBreedClassification.py
+++ DogBreedClassification.py
@@ -1,4 +1,4 @@
-import pandas as pd
+# import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Dropout, Flatten, Dense
 from tensorflow.keras.models import Model
@@ -18,40 +18,52 @@ datagen_test = ImageDataGenerator(rescale=1/255)
 
 train_images =  datagen.flow_from_directory(
         './trainDirectory/',
-        target_size=(224, 224),
+        target_size=(150, 150),
         batch_size=32,
         class_mode='categorical',
         subset = 'training')
 
 val_images =  datagen.flow_from_directory(
         './trainDirectory/',
-        target_size=(224, 224),
+        target_size=(150, 150),
         batch_size=32,
         class_mode='categorical',
         subset = 'validation')
 
 test_images = datagen_test.flow_from_directory(
         './',
-        target_size=(224, 224),
+        target_size=(150, 150),
         batch_size=32,
         class_mode=None,
         classes=['test'])
         
 input_tensor = Input(shape=(224,224,3))        
-model_resnet = ResNet50(include_top=False, input_tensor=input_tensor,input_shape=(224,224,3),weights = None, classes=120)
+# model_resnet = ResNet50(include_top=False, input_tensor=input_tensor,input_shape=(224,224,3),weights = None, classes=120)
 
-print(model_resnet.summary())
-conv1 = Conv2D(256, (3,3), activation = 'relu')(model_resnet.output)
-pool1 = MaxPooling2D(2,2)(conv1)
-drop1 = Dropout(0.2)(pool1)
+# print(model_resnet.summary())
+# conv1 = Conv2D(256, (3,3), activation = 'relu')(model_resnet.output)
+# pool1 = MaxPooling2D(2,2)(conv1)
+# drop1 = Dropout(0.2)(pool1)
 
-flatten1 = Flatten()(drop1)
-fc2 = Dense(120, activation='softmax')(flatten1)
+# flatten1 = Flatten()(drop1)
+# fc2 = Dense(120, activation='softmax')(flatten1)
 
-model = Model(inputs=model_resnet.input, outputs=fc2)
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(256, (3,3), activation='relu', input_shape=(150, 150, 3), padding = 'same'),
+  tf.keras.layers.MaxPooling2D((2, 2), padding = 'same'),
+  tf.keras.layers.Dropout(rate = 0.4),
+  tf.keras.layers.BatchNormalization(),
+  tf.keras.layers.SeparableConv2D(256, (3, 3), activation = 'relu',padding='same', dilation_rate=(2, 2)),
+  tf.keras.layers.MaxPooling2D((2,2), padding = 'same'),
+  tf.keras.layers.Dropout(rate = 0.4),
+  tf.keras.layers.BatchNormalization(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(120, activation='softmax')
+])
 
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['acc'])
-# model.fit(train_images, epochs = 20, batch_size = 64)
+model.fit(train_images, epochs = 20, batch_size = 64)
 print(model.summary())
 # model.fit_generator(
 #     train_images,
