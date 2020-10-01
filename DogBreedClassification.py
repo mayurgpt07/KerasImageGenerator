bec04ba143d372d037fc776470b54cfa41ac6de8
mayurgpt07 <mayurgpt07@gmail.com>
2020-10-01 14:15:07 -0700

    Working models established

--- DogBreedClassification.py
+++ DogBreedClassification.py
@@ -36,11 +36,11 @@ test_images = datagen_test.flow_from_directory(
         batch_size=32,
         class_mode=None,
         classes=['test'])
-        
-input_tensor = Input(shape=(224,224,3))        
-# model_resnet = ResNet50(include_top=False, input_tensor=input_tensor,input_shape=(224,224,3),weights = None, classes=120)
 
-# print(model_resnet.summary())
+## Resnet model        
+# input_tensor = Input(shape=(150,150,3))        
+# model_resnet = ResNet50(include_top=False, input_tensor=input_tensor,input_shape=(150,150,3),weights = 'imagenet', classes = 120)
+
 # conv1 = Conv2D(256, (3,3), activation = 'relu')(model_resnet.output)
 # pool1 = MaxPooling2D(2,2)(conv1)
 # drop1 = Dropout(0.2)(pool1)
@@ -48,17 +48,17 @@ input_tensor = Input(shape=(224,224,3))
 # flatten1 = Flatten()(drop1)
 # fc2 = Dense(120, activation='softmax')(flatten1)
 
+# model = Model(inputs = input_tensor, outputs = fc2)
+# print(model.summary())
+
+## Custom model for identification 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv2D(512, (3,3), activation='relu', input_shape=(150, 150, 3), padding = 'same'),
-  tf.keras.layers.MaxPooling2D((2, 2), padding = 'same'),
+  tf.keras.layers.Conv2D(512, (3,3), activation='relu', input_shape=(150, 150, 3)),
+  tf.keras.layers.MaxPooling2D((2, 2)),
   tf.keras.layers.Dropout(rate = 0.4),
   tf.keras.layers.BatchNormalization(),
-  tf.keras.layers.Conv2D(512, (3, 3), activation = 'relu',padding='same', dilation_rate=(2, 2)),
-  tf.keras.layers.MaxPooling2D((2,2), padding = 'same'),
-  tf.keras.layers.Dropout(rate = 0.4),
-  tf.keras.layers.BatchNormalization(),
-  tf.keras.layers.Conv2D(256, (3, 3), activation = 'relu',padding='same', dilation_rate=(2, 2)),
-  tf.keras.layers.MaxPooling2D((2,2), padding = 'same'),
+  tf.keras.layers.Conv2D(512, (3, 3), activation = 'relu'),
+  tf.keras.layers.MaxPooling2D((2,2)),
   tf.keras.layers.Dropout(rate = 0.4),
   tf.keras.layers.BatchNormalization(),
   tf.keras.layers.Flatten(),
@@ -69,9 +69,3 @@ model = tf.keras.models.Sequential([
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['acc'])
 model.fit(train_images, epochs = 40, batch_size = 128, validation_data = val_images)
 print(model.summary())
-# model.fit_generator(
-#     train_images,
-#     steps_per_epoch = 700,
-#     validation_data = val_images, 
-#     validation_steps = 300,
-#     epochs = 20)
